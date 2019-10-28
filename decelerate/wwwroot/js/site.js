50fda2d180fc79f03d8e19f0564b5b1f0ab9ec84
Robert Knauer <robert@capsaicin-dev.de>
2019-10-28 09:24:34 +0100

    add main slider to user area

--- decelerate/wwwroot/js/site.js
+++ decelerate/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿(function () {
+    window.addEventListener('load', function () {
+        /* Register event for slider change: */
+        $('#sliderInput').change(function () {
+            console.log($(this).val());
+        });
+    }, false);
+})();
